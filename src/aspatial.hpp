bc3ea7950f93c46c9fdf0120b0d7dc1f241e3f16
Aditya Kashi <adityakashi65@gmail.com>
2017-03-30 00:49:59 -0400

    made InviscidFlow methods virtual

--- src/aspatial.hpp
+++ src/aspatial.hpp
@@ -31,10 +31,10 @@
 namespace acfd {
 
 /// Base class for spatial discretization and integration of weak forms of PDEs
-/** 
+/**
  * Provides residual computation, and potentially residual Jacobian evaluation, interface for all solvers.
  * The template parameter nvars is the number of variables in the PDE system.
- * \note Make sure compute_topological(), compute_face_data() and compute_jacobians() have been called on the mesh object prior to initialzing an object of any subclass.
+ * \note Make sure compute_topological() has been called on the mesh object prior to initialzing an object of any subclass.
  */
 template <int nvars>
 class SpatialBase
@@ -47,13 +47,13 @@ protected:
 
 	/// stores (for each cell i) \f$ \sum_{j \in \partial\Omega_I} \int_j( |v_n| + c) d \Gamma \f$, where v_n and c are average values of the cell faces
 	std::vector<acfd_real> integ;
-	
+
 	Quadrature2DTriangle* dtquad;				///< Domain quadrature context
 	Quadrature2DSquare* dsquad;					///< Domain quadrature context
 	Quadrature1D* bquad;						///< Boundary quadrature context
 	Element_PhysicalElement* elems;				///< List of finite elements
 	FaceElement_PhysicalSpace* faces;			///< List of face elements
-	
+
 	/// Integral of fluxes across each face for all dofs
 	/** The entries corresponding to different DOFs of a given flow variable are stored contiguously.
 	 */
@@ -70,10 +70,10 @@ protected:
 	/* Reconstruction-related stuff - currently not implemented
 	//Reconstruction* rec;						///< Reconstruction context
 	//FaceDataComputation* lim;					///< Limiter context
-	
+
 	// Ghost cell centers
 	amat::Array2d<acfd_real> ghc;
-	
+
 	/// Ghost elements' flow quantities
 	std::vector<Vector> ug;
 
@@ -90,9 +90,9 @@ public:
 	 * \param _p_degree is the polynomial degree for FE basis functions
 	 */
 	SpatialBase(const UMesh2dh* mesh, const int _p_degree);
-	
+
 	virtual ~SpatialBase();
-	
+
 	/// Compute all finite element data, including mass matrix, needed for the spatial discretization
 	void computeFEData();
 
@@ -127,6 +127,10 @@ public:
 	virtual const amat::Array2d<acfd_real>& getoutput() const = 0;
 };
 
+/// Spatial discretization for 2D Euler equations
+/** Most functions are virtual so that other solvers can be subclassed
+ * and the functionality here can be selectively used.
+ */
 class InviscidFlow : public SpatialBase<4>
 {
 protected:
@@ -137,7 +141,7 @@ protected:
 
 	/// Add contribution of inviscid numerical flux to flux storage
 	void inviscidFluxContribution();
-	
+
 	/// Computes flow variables at boundaries (either Gauss points or ghost cell centers) using the interior state provided
 	/** \param[in] instates provides the left (interior state) for each boundary face
 	 * \param[out] bounstates will contain the right state of boundary faces
@@ -145,21 +149,21 @@ protected:
 	 * Currently does not use characteristic BCs.
 	 * \todo Implement and test characteristic BCs
 	 */
-	void compute_boundary_states(const std::vector<Vector>& instates, std::vector<Vector>& bounstates);
+	virtual void compute_boundary_states(const std::vector<Vector>& instates, std::vector<Vector>& bounstates);
 
 public:
 	/// Calls functions to assemble the [right hand side](@ref residual)
-	void compute_residual();
+	virtual void compute_residual();
 
 	/// Compute norm of entropy production
 	/// Call after computing pressure etc \sa postprocess_cell
 	acfd_real compute_entropy();
-	
+
 	/// Compute nodal quantities to export, based on area-weighted averaging (which takes into account ghost cells as well)
-	virtual void postprocess() = 0;
+	virtual void postprocess();
 
 	/// Read-only access to output quantities
-	virtual const amat::Array2d<acfd_real>& getoutput() const = 0;
+	virtual const amat::Array2d<acfd_real>& getoutput() const;
 };
 
 }	// end namespace
